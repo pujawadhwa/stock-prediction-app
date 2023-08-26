79475b187d5e8e5d68b3e24732245a8b140602aa
pujawadhwa <110151374+pujawadhwa@users.noreply.github.com>
2023-08-26 19:36:18 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@
 # from sklearn.preprocessing import MinMaxScaler 
 
 # start = '2010-01-01'
-# end = '2020-01-01'
+# end = '2023-08-01'
 
 # st.title('Stock Trend Prediction')
 
@@ -51,7 +51,7 @@
 # data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.70)])
 # data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.70):int(len(df))])
 
-# scaler = MinMaxScaler(feature_range=(0, 1))
+# scaler = MinMaxScaler(feature_range=(0,1 ))
 # data_training_array = scaler.fit_transform(data_training)
 
 # # x_train = []
@@ -70,7 +70,7 @@
 
 # final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
 
-# input_data = scaler.transform(final_df)
+# input_data = scaler.fit_transform(final_df)
 
 # x_test =[]
 # y_test =[]
@@ -82,7 +82,7 @@
 # x_test, y_test = np.array(x_test),np.array(y_test)
 # y_predicted = model.predict(x_test)
 
-# scale_factor = scaler.scale_[0]
+# scale_factor = 1/scaler.scale_[0]
 # y_predicted = y_predicted * scale_factor
 # y_test = y_test * scale_factor
 
@@ -95,33 +95,32 @@
 # plt.ylabel('Price')
 # plt.legend()
 # st.pyplot(fig2)
+
+
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 import yfinance as yf
-from keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
 
 # Load the trained model
-model = load_model('stock_trend_model.keras')
-
-# Define a function to predict stock prices
-def predict_stock_price(data, scaler):
-    input_data = scaler.transform(data)
-    x_test = []
-
-    for i in range(100, input_data.shape[0]):
-        x_test.append(input_data[i - 100:i])
-
-    x_test = np.array(x_test)
-    y_predicted = model.predict(x_test)
-
-    scale_factor = scaler.scale_[0]
-    y_predicted = y_predicted * scale_factor
-
-    return y_predicted
+model = Sequential([
+    LSTM(units=50, activation='relu', return_sequences=True, input_shape=(100, 1)),
+    Dropout(0.2),
+    LSTM(units=50, activation='relu', return_sequences=True),
+    Dropout(0.2),
+    LSTM(units=50, activation='relu'),
+    Dropout(0.2),
+    Dense(units=1)
+])
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
 
 # Streamlit UI
 st.title('Financial AI Chatbot')
@@ -129,7 +128,7 @@ st.title('Financial AI Chatbot')
 # User input
 user_input = st.text_input('Enter Stock Ticker', 'AAPL')
 start = '2010-01-01'
-end = '2020-01-01'
+end = '2020-01-01'  # Extend the time range to 2023
 
 # Download stock data
 df = yf.download(user_input, start=start, end=end)
@@ -138,17 +137,17 @@ if not df.empty:
     scaler = MinMaxScaler(feature_range=(0, 1))
     data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.70)])
     scaler.fit(data_training)
-    
+
     if st.button('Show Data Description'):
-        st.subheader('Data from 2010-2019')
+        st.subheader('Data from 2010-2023')
         st.write(df.describe())
-    
+
     if st.button('Show Closing Price vs Time Chart'):
         st.subheader('Closing Price vs Time Chart')
         fig = plt.figure(figsize=(12, 6))
         plt.plot(df['Close'])
         st.pyplot(fig)
-    
+
     if st.button('Show Closing Price vs Time Chart with Moving Averages'):
         ma100 = df['Close'].rolling(window=100).mean()
         ma200 = df['Close'].rolling(window=200).mean()
@@ -158,22 +157,44 @@ if not df.empty:
         plt.plot(df['Close'], label='Closing Price')
         plt.legend()
         st.pyplot(fig)
-    
-    if st.button('Show Predictions vs Original'):
-        past_100_days = data_training.tail(100)
-        final_df = pd.concat([past_100_days, df['Close']], ignore_index=True)
-        final_df.columns = final_df.columns.astype(str)  # Convert column names to strings
-        input_data = scaler.transform(final_df)
-        y_predicted = predict_stock_price(input_data, scaler)
 
-        st.subheader('Predictions vs Original')
-        fig2 = plt.figure(figsize=(12, 6))
-        plt.plot(df.index[-len(y_predicted):], df['Close'][-len(y_predicted):], 'b', label='Original Price')
-        plt.plot(df.index[-len(y_predicted):], y_predicted, 'r', label='Predicted Price')
-        plt.xlabel('Time')
-        plt.ylabel('Price')
-        plt.legend()
-        st.pyplot(fig2)
+if st.button('Show Predictions vs Original'):
+    past_100_days = data_training.tail(100)
+    
+    final_df = pd.concat([past_100_days, df['Close']], axis=0)
+    input_data = scaler.transform(final_df)
+    final_df.columns = final_df.columns.astype(str)
+    input_data = scaler.transform(final_df)
+    x_test = []
+    y_test = []
+
+    for i in range(100, input_data.shape[0]):
+        x_test.append(input_data[i - 100:i])
+        y_test.append(input_data[i, 0])
+
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+
+    # Reshape x_test to match the expected shape of the model input
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    y_predicted = model.predict(x_test)
+
+    # Inverse transform the predictions back to the original scale
+    y_predicted = scaler.inverse_transform(y_predicted)
+
+    # Inverse transform the original test data back to the original scale
+    y_test = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
+
+    st.subheader('Predictions vs Original')
+    fig2 = plt.figure(figsize=(12, 6))
+    plt.plot(df.index[-len(y_predicted):], y_test[-len(y_predicted):], 'b', label='Original Price')
+    plt.plot(df.index[-len(y_predicted):], y_predicted, 'r', label='Predicted Price')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend()
+    st.pyplot(fig2)
+
 
 
         # User input for questions
@@ -198,5 +219,12 @@ elif user_question.lower() == 'what was the highest stock price in the given per
 
 else:
     st.write("I'm sorry, I couldn't understand your question.")
+
+
+
+
+
+
+
 
 
